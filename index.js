2d4f945fc71042e2060c59e2a1ed8e874778e984
Beepboopitsjoop <beepboopitsthejoop@gmail.com>
2022-09-26 19:06:22 +0300

    Finished logic

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-const temp = document.querySelectorAll('[data-selector]')
+import data from "./data.json" assert { type: "json" };
+const contentTime = document.querySelectorAll('.content-time') // Used for DOM Update
 
-temp.forEach(elem => {
-    elem.addEventListener('click', changeSelector)
-});
+const init = (() => {
+    const selections = document.querySelectorAll('[data-selection]') // Daily, Weekly, Monthly
 
-let selector
+    const focusLastSelection = (() => {
+        if (localStorage.length === 0) localStorage.setItem('lastSelection', 'daily') // Prevents Error on Empty Local Storage
 
-function changeSelector(e) {
-    selector = e.target.dataset.selector
-    console.log(selector)
+        selections.forEach(elem => {
+            if (localStorage.lastSelection === elem.dataset.selection) elem.focus()
+        })
+    })()
+
+    selections.forEach(elem => {
+        elem.addEventListener('click', changeSelection)
+    });
+
+    function changeSelection(e) {
+        const lastSelection = localStorage.lastSelection
+        const currSelection = e.target.dataset.selection
+
+        if (lastSelection === currSelection) return
+        localStorage.setItem('lastSelection', currSelection)
+        updateDOM(currSelection)
+    }
+
+    updateDOM(localStorage.lastSelection)
+})()
+
+
+
+function updateDOM() {
+
+    const determinePreviousStr = (selection) => {
+        if (selection === 'daily') return 'Yesterday'
+        if (selection === 'weekly') return 'Last Week'
+        if (selection === 'monthly') return 'Last Month'
+    }
+
+    const determineHourNum = (hourNum) => {
+        if (hourNum == 1) return 'hr'
+        return 'hrs'
+    }
+
+    for (let i = 0; i < contentTime.length; i++) {
+        // Gets Needed Info from data.json According to Last Selection of Daily/Weekly/Monthly
+        let selectedData = data[i].timeframes[localStorage.lastSelection] 
+
+        const current = contentTime[i].children[0];
+        current.textContent = `${selectedData.current}${determineHourNum(selectedData.previous)}`
+
+        const previous = contentTime[i].children[1];
+        previous.textContent = `${determinePreviousStr(localStorage.lastSelection)} 
+        - ${selectedData.previous}${determineHourNum(selectedData.previous)}`
+    }
 }
 
